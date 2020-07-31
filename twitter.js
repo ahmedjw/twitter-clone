1132b2088ac45ad185b30e2d51d96b20350fe6c5
ahmed jwifel <39170585+ahmedjw@users.noreply.github.com>
2020-07-31 20:31:41 +0300

    Create twitter.js

---
+++ twitter.js
@@ -0,0 +1,48 @@
+var tweets = {
+    aut: "@ahmedjw",
+    twet: "",
+}
+var arr = new Array();
+var username = "";
+var neckname = "";
+
+var ttext = "";
+
+document.querySelector('.butT2').addEventListener('click', function () {
+
+
+    ttext = document.querySelector('.tweet').value;
+    document.querySelector('.tweet').value = "";
+    username = document.querySelector('.un').textContent;
+    neckname = document.querySelector('.nn').textContent;
+    var twt = document.querySelector('.Nfeed');
+    var clone = twt.cloneNode(true);
+    clone.id = 'Nfeed2';
+
+    clone.classList.add('text-large');
+    twt.before(clone);
+    document.querySelector('#tweets').textContent = ttext;
+    arr.push(tweets.aut);
+    tweets.twet = ttext;
+    arr.push(tweets.twet);
+
+
+});
+
+document.querySelector('#l').addEventListener('click', function () {
+    if (document.querySelector("#tweets").style.color == "black") {
+        document.querySelector("#tweets").style.color = "blue";
+    }
+    else {
+        document.querySelector("#tweets").style.color = "black";
+    }
+});
+
+document.querySelector('#r').addEventListener('click', function () {
+    var twt = document.querySelector('.Nfeed');
+    var c = twt.cloneNode(true);
+    c.id = 'Nfeed3';
+    c.classList.add('ret');
+    twt.before(c);
+
+});
